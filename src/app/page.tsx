63f8709b83eb039979ed2f80c25b181775ba286a
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-01-12 16:29:17 +0530

    Feat: Renamed Header Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import Image from "next/image";
-import Header from "./ui/Header/header";
 import Sidebar from "./ui/Sidebar/Sidebar";
 import { useState } from "react";
+import CustomHeader from "./ui/HeaderComponent/CustomHeader";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="bg-white flex w-screen h-full">
-      <Header setIsOpen={setIsOpen} isOpen={isOpen}></Header>
-
-      <Sidebar setIsOpen={setIsOpen} isOpen={isOpen}></Sidebar>
+    <div>
+      <div className="bg-white flex w-screen h-full">
+        <CustomHeader setIsOpen={setIsOpen} isOpen={isOpen}></CustomHeader>
+        <Sidebar setIsOpen={setIsOpen} isOpen={isOpen}></Sidebar>
+      </div>
     </div>
   );
 }
