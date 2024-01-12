831af5fa1b5d7f0e9eed7b98aa6d9dc944587061
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-01-12 13:41:10 +0530

    Feat : Added header

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -1,3 +1,3 @@
-import { Inter } from 'next/font/google';
+import { Sora } from 'next/font/google';
  
-export const inter = Inter({ subsets: ['latin'] });
+export const sora = Sora({ subsets: ['latin'] });
--- src/app/ui/header/header.tsx
+++ src/app/ui/header/header.tsx
@@ -1,10 +1,13 @@
+"use client";
 import Image from "next/image";
+import { sora } from "../fonts";
 
 export default function Header() {
+  const headers = ["About", "Skills", "Projects", "Contact me"];
   return (
-    <div className="px-20 py-6">
-      <div className="bg-gray-200 px-8 flex justify-between">
-        <div>
+    <div className="bg-pink-50 px-2   lg:px-12 xl:px-16  2xl:px-20 py-6 ">
+      <div className=" md:px-8 flex justify-between items-center">
+        <div className="flex justify-center items-center space-x-1">
           <Image
             src="/logo-1.png"
             className=" rounded-full my-2"
@@ -12,13 +15,32 @@ export default function Header() {
             width={24}
             height={24}
           />
+          <div className="text-sm italic ">Hello</div>
+        </div>
+        <div className="flex justify-center items-center space-x-8">
+          {headers.map((eachHeader) => (
+            <button className=" py-4 px-3 hover:shadow-[0_14px_12px_-12px_rgba(0,0,0,0.4)] text-sm leading-6 capitalize font-semibold tracking-[-.4px]">
+              {eachHeader}
+            </button>
+          ))}
         </div>
 
-        <div>Hi</div>
-
-        <div>
-          <button>Resume</button>
-        </div>
+        <button
+          className="rounded bg-black text-white py-4 px-5 flex space-x-2 justify-center"
+          onClick={() => {
+            console.log("Resume downloaded");
+          }}
+        >
+          <div className={`text-sm leading-6 font-semibold ${sora.className}`}>
+            Resume
+          </div>
+          <Image
+            src={"/download.png"}
+            alt={"Download image"}
+            height={20}
+            width={20}
+          ></Image>
+        </button>
       </div>
     </div>
   );
