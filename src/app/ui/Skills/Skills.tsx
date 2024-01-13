fa4b2cc34e383b599f7e7686bfc0ee55ea63d32d
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-01-13 13:33:39 +0530

    Feat: Added icons

--- src/app/ui/Skills/Skills.tsx
+++ src/app/ui/Skills/Skills.tsx
@@ -17,8 +17,78 @@ import SkillComponent from "../components/SkillComponent/SkillComponent";
 // Xamarin Forms
 // Version Control:
 // Git
-
+[
+  "Git",
+  "ReactJS",
+  "React Native",
+  "NextJS",
+  "Javascript",
+  "Typescript",
+  "Storybook",
+  "Builder.io",
+  "Xamarin Forms",
+  "C#",
+  "Python",
+];
 export default function Skills() {
+  const skillsList = [
+    {
+      name: "Git",
+      icon: "icon-git.png",
+    },
+    {
+      name: "ReactJS",
+      icon: "icon-react.png",
+    },
+    {
+      name: "React Native",
+      icon: "icon-react.png",
+    },
+    {
+      name: "NextJS",
+      icon: "icon-nextjs.png",
+    },
+    {
+      name: "Javascript",
+      icon: "icon-js.png",
+    },
+    {
+      name: "Typescript",
+      icon: "icon-ts.png",
+    },
+    {
+      name: "Storybook",
+      icon: "icon-storybook.png",
+    },
+    {
+      name: "Builder.io",
+      icon: "icon-builder.png",
+    },
+    {
+      name: "Xamarin Forms",
+      icon: "icon-xamarin.png",
+    },
+    {
+      name: "C#",
+      icon: "icon-csharp.png",
+    },
+    {
+      name: "Python",
+      icon: "icon-python.png",
+    },
+    {
+      name: "Scss",
+      icon: "icon-sass.png",
+    },
+    {
+      name: "Styled components",
+      icon: "icon-styledcomponents.png",
+    },
+    {
+      name: "Tailwind css",
+      icon: "icon-tailwind.png",
+    },
+  ];
   return (
     <div className="mb-8">
       <div className="flex gap-1 justify-center items-center">
@@ -31,21 +101,12 @@ export default function Skills() {
       </div>
       <div className="flex justify-center items-center">
         <div className=" w-full lg:mx-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 3xl:grid-cols-8 gap-x-3 gap-y-4 ">
-          {[
-            "Git",
-            "ReactJS",
-            "React Native",
-            "NextJS",
-            "Javascript",
-            "Typescript",
-            "Storybook",
-            "Builder.io",
-            "Xamarin Forms",
-            "C#",
-            "Python",
-          ].map((eachTitle) => (
-            <div key={eachTitle}>
-              <SkillComponent skillName={eachTitle}></SkillComponent>
+          {skillsList.map((eachSkill) => (
+            <div key={eachSkill.name}>
+              <SkillComponent
+                skillName={eachSkill.name}
+                url={eachSkill.icon}
+              ></SkillComponent>
             </div>
           ))}
         </div>
--- src/app/ui/components/SkillComponent/SkillComponent.tsx
+++ src/app/ui/components/SkillComponent/SkillComponent.tsx
@@ -2,11 +2,14 @@
 
 import ImageComponent from "../ImageComponent/ImageComponent";
 
-export default function SkillComponent(props: { skillName: string }) {
+export default function SkillComponent(props: {
+  skillName: string;
+  url: string;
+}) {
   return (
     <div className=" items-center shadow-md w-full aspect-square flex hover:shadow-2xl hover:bg-pink-100 flex-col justify-between p-6  bg-pink-50  border-[2px] text-center border-pink-200">
       <ImageComponent
-        title={"icon-git.png"}
+        title={props.url}
         alt={props.skillName}
         width={56}
         height={56}
