17ee4fd03b316d3d955f663ea7cfc25e801c6e4d
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-01-13 15:09:29 +0530

    Fix: Removed second bg color for category button

--- src/app/ui/components/SkillCategories/SkillCategories.tsx
+++ src/app/ui/components/SkillCategories/SkillCategories.tsx
@@ -22,7 +22,11 @@ export default function SkillCategories(props: {
     <div className="w-full">
       <div className="grid grid-cols-2 xs:grid:cols-2 sm:grid-cols-3 md:grid-cols-4 flex-col items-start gap-4  ">
         {categoryList.map((eachCategory: skillCategoryInterface) => {
-          console.log(eachCategory.category, selectedCategory);
+          console.log(
+            eachCategory.category,
+            selectedCategory,
+            selectedCategory === eachCategory.category
+          );
           return (
             <button
               key={eachCategory.category}
@@ -30,10 +34,10 @@ export default function SkillCategories(props: {
                 onClickCategory(eachCategory.category, eachCategory.list);
               }}
               className={`p-3 ${
-                selectedCategory == eachCategory.category
+                selectedCategory === eachCategory.category
                   ? "bg-pink-200"
-                  : "bg-pinl-50"
-              } bg-pink-50  aspect-video flex justify-center items-center text-sm`}
+                  : "bg-pink-50"
+              }   aspect-video flex justify-center items-center text-sm`}
             >
               {eachCategory.category}
             </button>
