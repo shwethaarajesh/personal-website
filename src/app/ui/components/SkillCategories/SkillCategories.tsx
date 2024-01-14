e08cd89dc1ff0e4e9254a67b256c8dd000a73a34
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-01-14 22:01:49 +0530

    Feat : Added images

--- src/app/ui/components/SkillCategories/SkillCategories.tsx
+++ src/app/ui/components/SkillCategories/SkillCategories.tsx
@@ -48,7 +48,7 @@ export default function SkillCategories(props: {
       <TransitionGroup
         className="lg:px-12 border my-4 py-4 px-4 md:px-6 grid grid-cols-2 sm:grid-cols-3
                          md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 
-                         2xl:grid-cols-7 3xl:grid-cols-8 gap-x-6 gap-y-6 lg:gap-x-7 xl:gap-x-8  lg:gap-y-7"
+                         2xl:grid-cols-7 3xl:grid-cols-8 gap-x-5 md:gap-x-6 gap-y-6 lg:gap-x-7 xl:gap-x-8  lg:gap-y-7"
       >
         {skillList.map((eachSkill) => (
           <CSSTransition key={eachSkill.name} timeout={300} classNames="fade">
