bb572224e128209f4fc46f1e1df9ef370abe1212
Shwethaa Rajesh <shwethaarajesh14@gmail.com>
2024-10-03 17:45:41 -0400

    Feat: Added logo and scroll to top

--- src/app/ui/HeaderComponent/CustomHeader.tsx
+++ src/app/ui/HeaderComponent/CustomHeader.tsx
@@ -3,7 +3,15 @@ import { MdOutlineMenu } from "react-icons/md";
 import TabButton from "../components/TabButton/TabButton";
 import DownloadButton from "../components/DownloadButton/DownloadButton";
 import ImageComponent from "../components/ImageComponent/ImageComponent";
+import { Great_Vibes } from 'next/font/google'
 import { MutableRefObject, RefObject } from "react";
+
+const greatvibes = Great_Vibes({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
 
 export default function CustomHeader(props: {
   isOpen: boolean;
@@ -13,22 +21,27 @@ export default function CustomHeader(props: {
     ref: RefObject<HTMLDivElement>;
   }[];
 }) {
+
+  const isBrowser = () => typeof window !== 'undefined';
   return (
     <div
-      className={` fixed w-full h-[100px] border-b shadow-md shadow-sky-50 border-b-white bg-primary px-2 flex  lg:px-12 xl:px-16  2xl:px-20 py-6 text-white `}
+      className={` fixed w-full h-[100px] border-b shadow-md shadow-sky-50 border-b-white bg-primary pl-8 pr-4 md:px-2  flex  lg:px-12 xl:px-16  2xl:px-20 py-6 text-white `}
     >
       <div className={`md:px-8 flex justify-between items-center w-full `}>
-        <div className="flex justify-center items-center space-x-1">
+        <div className="flex justify-center items-center space-x-1 text-md lg:text-2xl cursor-pointer" onClick={()=>{
+          if (!isBrowser()) return;
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }}>
           {/* <ImageComponent
-            title="Logo-name.png"
+            title="name-logo.png"
             className=" rounded-full my-2"
             alt="Shwethaa Rajesh"
             width={190}
             height={190}
           /> */}
-          {/* <div className="text-sm italic ">Hello</div> */}
+          <div className={greatvibes.className}>Shwethaa</div>
         </div>
-        <div className=" hidden md:flex justify-center items-center space-x-8">
+        <div className=" hidden md:flex justify-center items-center space-x-4 lg:space-x-8">
           {props.headers.map((eachHeader) => (
             <div
               key={eachHeader.name}
